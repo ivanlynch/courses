03cd1b5082727164ee9a47a66bcc4c4ccbccd5b6
Iván Lynch <ivan.lynch@outlook.com.ar>
2020-05-17 20:38:55 -0300

    Updates

---
+++ build/bundle.js
@@ -0,0 +1,626 @@
+var app = (function () {
+    'use strict';
+
+    function noop() { }
+    function assign(tar, src) {
+        // @ts-ignore
+        for (const k in src)
+            tar[k] = src[k];
+        return tar;
+    }
+    function run(fn) {
+        return fn();
+    }
+    function blank_object() {
+        return Object.create(null);
+    }
+    function run_all(fns) {
+        fns.forEach(run);
+    }
+    function is_function(thing) {
+        return typeof thing === 'function';
+    }
+    function safe_not_equal(a, b) {
+        return a != a ? b == b : a !== b || ((a && typeof a === 'object') || typeof a === 'function');
+    }
+    function create_slot(definition, ctx, $$scope, fn) {
+        if (definition) {
+            const slot_ctx = get_slot_context(definition, ctx, $$scope, fn);
+            return definition[0](slot_ctx);
+        }
+    }
+    function get_slot_context(definition, ctx, $$scope, fn) {
+        return definition[1] && fn
+            ? assign($$scope.ctx.slice(), definition[1](fn(ctx)))
+            : $$scope.ctx;
+    }
+    function get_slot_changes(definition, $$scope, dirty, fn) {
+        if (definition[2] && fn) {
+            const lets = definition[2](fn(dirty));
+            if ($$scope.dirty === undefined) {
+                return lets;
+            }
+            if (typeof lets === 'object') {
+                const merged = [];
+                const len = Math.max($$scope.dirty.length, lets.length);
+                for (let i = 0; i < len; i += 1) {
+                    merged[i] = $$scope.dirty[i] | lets[i];
+                }
+                return merged;
+            }
+            return $$scope.dirty | lets;
+        }
+        return $$scope.dirty;
+    }
+
+    function append(target, node) {
+        target.appendChild(node);
+    }
+    function insert(target, node, anchor) {
+        target.insertBefore(node, anchor || null);
+    }
+    function detach(node) {
+        node.parentNode.removeChild(node);
+    }
+    function element(name) {
+        return document.createElement(name);
+    }
+    function text(data) {
+        return document.createTextNode(data);
+    }
+    function space() {
+        return text(' ');
+    }
+    function attr(node, attribute, value) {
+        if (value == null)
+            node.removeAttribute(attribute);
+        else if (node.getAttribute(attribute) !== value)
+            node.setAttribute(attribute, value);
+    }
+    function children(element) {
+        return Array.from(element.childNodes);
+    }
+
+    let current_component;
+    function set_current_component(component) {
+        current_component = component;
+    }
+
+    const dirty_components = [];
+    const binding_callbacks = [];
+    const render_callbacks = [];
+    const flush_callbacks = [];
+    const resolved_promise = Promise.resolve();
+    let update_scheduled = false;
+    function schedule_update() {
+        if (!update_scheduled) {
+            update_scheduled = true;
+            resolved_promise.then(flush);
+        }
+    }
+    function add_render_callback(fn) {
+        render_callbacks.push(fn);
+    }
+    let flushing = false;
+    const seen_callbacks = new Set();
+    function flush() {
+        if (flushing)
+            return;
+        flushing = true;
+        do {
+            // first, call beforeUpdate functions
+            // and update components
+            for (let i = 0; i < dirty_components.length; i += 1) {
+                const component = dirty_components[i];
+                set_current_component(component);
+                update(component.$$);
+            }
+            dirty_components.length = 0;
+            while (binding_callbacks.length)
+                binding_callbacks.pop()();
+            // then, once components are updated, call
+            // afterUpdate functions. This may cause
+            // subsequent updates...
+            for (let i = 0; i < render_callbacks.length; i += 1) {
+                const callback = render_callbacks[i];
+                if (!seen_callbacks.has(callback)) {
+                    // ...so guard against infinite loops
+                    seen_callbacks.add(callback);
+                    callback();
+                }
+            }
+            render_callbacks.length = 0;
+        } while (dirty_components.length);
+        while (flush_callbacks.length) {
+            flush_callbacks.pop()();
+        }
+        update_scheduled = false;
+        flushing = false;
+        seen_callbacks.clear();
+    }
+    function update($$) {
+        if ($$.fragment !== null) {
+            $$.update();
+            run_all($$.before_update);
+            const dirty = $$.dirty;
+            $$.dirty = [-1];
+            $$.fragment && $$.fragment.p($$.ctx, dirty);
+            $$.after_update.forEach(add_render_callback);
+        }
+    }
+    const outroing = new Set();
+    let outros;
+    function transition_in(block, local) {
+        if (block && block.i) {
+            outroing.delete(block);
+            block.i(local);
+        }
+    }
+    function transition_out(block, local, detach, callback) {
+        if (block && block.o) {
+            if (outroing.has(block))
+                return;
+            outroing.add(block);
+            outros.c.push(() => {
+                outroing.delete(block);
+                if (callback) {
+                    if (detach)
+                        block.d(1);
+                    callback();
+                }
+            });
+            block.o(local);
+        }
+    }
+    function create_component(block) {
+        block && block.c();
+    }
+    function mount_component(component, target, anchor) {
+        const { fragment, on_mount, on_destroy, after_update } = component.$$;
+        fragment && fragment.m(target, anchor);
+        // onMount happens before the initial afterUpdate
+        add_render_callback(() => {
+            const new_on_destroy = on_mount.map(run).filter(is_function);
+            if (on_destroy) {
+                on_destroy.push(...new_on_destroy);
+            }
+            else {
+                // Edge case - component was destroyed immediately,
+                // most likely as a result of a binding initialising
+                run_all(new_on_destroy);
+            }
+            component.$$.on_mount = [];
+        });
+        after_update.forEach(add_render_callback);
+    }
+    function destroy_component(component, detaching) {
+        const $$ = component.$$;
+        if ($$.fragment !== null) {
+            run_all($$.on_destroy);
+            $$.fragment && $$.fragment.d(detaching);
+            // TODO null out other refs, including component.$$ (but need to
+            // preserve final state?)
+            $$.on_destroy = $$.fragment = null;
+            $$.ctx = [];
+        }
+    }
+    function make_dirty(component, i) {
+        if (component.$$.dirty[0] === -1) {
+            dirty_components.push(component);
+            schedule_update();
+            component.$$.dirty.fill(0);
+        }
+        component.$$.dirty[(i / 31) | 0] |= (1 << (i % 31));
+    }
+    function init(component, options, instance, create_fragment, not_equal, props, dirty = [-1]) {
+        const parent_component = current_component;
+        set_current_component(component);
+        const prop_values = options.props || {};
+        const $$ = component.$$ = {
+            fragment: null,
+            ctx: null,
+            // state
+            props,
+            update: noop,
+            not_equal,
+            bound: blank_object(),
+            // lifecycle
+            on_mount: [],
+            on_destroy: [],
+            before_update: [],
+            after_update: [],
+            context: new Map(parent_component ? parent_component.$$.context : []),
+            // everything else
+            callbacks: blank_object(),
+            dirty
+        };
+        let ready = false;
+        $$.ctx = instance
+            ? instance(component, prop_values, (i, ret, ...rest) => {
+                const value = rest.length ? rest[0] : ret;
+                if ($$.ctx && not_equal($$.ctx[i], $$.ctx[i] = value)) {
+                    if ($$.bound[i])
+                        $$.bound[i](value);
+                    if (ready)
+                        make_dirty(component, i);
+                }
+                return ret;
+            })
+            : [];
+        $$.update();
+        ready = true;
+        run_all($$.before_update);
+        // `false` as a special case of no DOM component
+        $$.fragment = create_fragment ? create_fragment($$.ctx) : false;
+        if (options.target) {
+            if (options.hydrate) {
+                const nodes = children(options.target);
+                // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
+                $$.fragment && $$.fragment.l(nodes);
+                nodes.forEach(detach);
+            }
+            else {
+                // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
+                $$.fragment && $$.fragment.c();
+            }
+            if (options.intro)
+                transition_in(component.$$.fragment);
+            mount_component(component, options.target, options.anchor);
+            flush();
+        }
+        set_current_component(parent_component);
+    }
+    class SvelteComponent {
+        $destroy() {
+            destroy_component(this, 1);
+            this.$destroy = noop;
+        }
+        $on(type, callback) {
+            const callbacks = (this.$$.callbacks[type] || (this.$$.callbacks[type] = []));
+            callbacks.push(callback);
+            return () => {
+                const index = callbacks.indexOf(callback);
+                if (index !== -1)
+                    callbacks.splice(index, 1);
+            };
+        }
+        $set() {
+            // overridden by instance, if it has props
+        }
+    }
+
+    /* src/components/Header.svelte generated by Svelte v3.22.2 */
+
+    function create_fragment(ctx) {
+    	let div3;
+
+    	return {
+    		c() {
+    			div3 = element("div");
+
+    			div3.innerHTML = `<div class="hero-image svelte-ewqrci"><div class="header-bar"><img class="header-logo svelte-ewqrci" src="/img/tierra.svg" alt="Courses Market Online"> 
+      <ul class="header-menu svelte-ewqrci"><li class="svelte-ewqrci"><a class="header-menu-items svelte-ewqrci" href="#/Ingresar">Ingresar</a></li> 
+        <li class="svelte-ewqrci"><a class="header-menu-items svelte-ewqrci" href="#/Registro">Registrarse</a></li></ul></div> 
+    <div class="hero-text svelte-ewqrci"><h1>E-Courses</h1> 
+      <p class="hero-subtitle svelte-ewqrci">The place where you find whatever you need</p></div> 
+    <svg class="hero-svg svelte-ewqrci" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320"><path fill="#FFCA18" fill-opacity="1" d="M0,32L48,64C96,96,192,160,288,170.7C384,181,480,139,576,101.3C672,64,768,32,864,69.3C960,107,1056,213,1152,218.7C1248,224,1344,128,1392,80L1440,32L1440,320L1392,320C1344,320,1248,320,1152,320C1056,320,960,320,864,320C768,320,672,320,576,320C480,320,384,320,288,320C192,320,96,320,48,320L0,320Z"></path></svg></div>`;
+
+    			attr(div3, "class", "header-container svelte-ewqrci");
+    		},
+    		m(target, anchor) {
+    			insert(target, div3, anchor);
+    		},
+    		p: noop,
+    		i: noop,
+    		o: noop,
+    		d(detaching) {
+    			if (detaching) detach(div3);
+    		}
+    	};
+    }
+
+    class Header extends SvelteComponent {
+    	constructor(options) {
+    		super();
+    		init(this, options, null, create_fragment, safe_not_equal, {});
+    	}
+    }
+
+    /* src/components/Content.svelte generated by Svelte v3.22.2 */
+
+    const get_footer_slot_changes = dirty => ({});
+    const get_footer_slot_context = ctx => ({});
+    const get_body_slot_changes = dirty => ({});
+    const get_body_slot_context = ctx => ({});
+    const get_header_slot_changes = dirty => ({});
+    const get_header_slot_context = ctx => ({});
+
+    function create_fragment$1(ctx) {
+    	let div;
+    	let t0;
+    	let t1;
+    	let current;
+    	const header_slot_template = /*$$slots*/ ctx[1].header;
+    	const header_slot = create_slot(header_slot_template, ctx, /*$$scope*/ ctx[0], get_header_slot_context);
+    	const body_slot_template = /*$$slots*/ ctx[1].body;
+    	const body_slot = create_slot(body_slot_template, ctx, /*$$scope*/ ctx[0], get_body_slot_context);
+    	const footer_slot_template = /*$$slots*/ ctx[1].footer;
+    	const footer_slot = create_slot(footer_slot_template, ctx, /*$$scope*/ ctx[0], get_footer_slot_context);
+
+    	return {
+    		c() {
+    			div = element("div");
+    			if (header_slot) header_slot.c();
+    			t0 = space();
+    			if (body_slot) body_slot.c();
+    			t1 = space();
+    			if (footer_slot) footer_slot.c();
+    			attr(div, "class", "container svelte-w9g1xq");
+    		},
+    		m(target, anchor) {
+    			insert(target, div, anchor);
+
+    			if (header_slot) {
+    				header_slot.m(div, null);
+    			}
+
+    			append(div, t0);
+
+    			if (body_slot) {
+    				body_slot.m(div, null);
+    			}
+
+    			append(div, t1);
+
+    			if (footer_slot) {
+    				footer_slot.m(div, null);
+    			}
+
+    			current = true;
+    		},
+    		p(ctx, [dirty]) {
+    			if (header_slot) {
+    				if (header_slot.p && dirty & /*$$scope*/ 1) {
+    					header_slot.p(get_slot_context(header_slot_template, ctx, /*$$scope*/ ctx[0], get_header_slot_context), get_slot_changes(header_slot_template, /*$$scope*/ ctx[0], dirty, get_header_slot_changes));
+    				}
+    			}
+
+    			if (body_slot) {
+    				if (body_slot.p && dirty & /*$$scope*/ 1) {
+    					body_slot.p(get_slot_context(body_slot_template, ctx, /*$$scope*/ ctx[0], get_body_slot_context), get_slot_changes(body_slot_template, /*$$scope*/ ctx[0], dirty, get_body_slot_changes));
+    				}
+    			}
+
+    			if (footer_slot) {
+    				if (footer_slot.p && dirty & /*$$scope*/ 1) {
+    					footer_slot.p(get_slot_context(footer_slot_template, ctx, /*$$scope*/ ctx[0], get_footer_slot_context), get_slot_changes(footer_slot_template, /*$$scope*/ ctx[0], dirty, get_footer_slot_changes));
+    				}
+    			}
+    		},
+    		i(local) {
+    			if (current) return;
+    			transition_in(header_slot, local);
+    			transition_in(body_slot, local);
+    			transition_in(footer_slot, local);
+    			current = true;
+    		},
+    		o(local) {
+    			transition_out(header_slot, local);
+    			transition_out(body_slot, local);
+    			transition_out(footer_slot, local);
+    			current = false;
+    		},
+    		d(detaching) {
+    			if (detaching) detach(div);
+    			if (header_slot) header_slot.d(detaching);
+    			if (body_slot) body_slot.d(detaching);
+    			if (footer_slot) footer_slot.d(detaching);
+    		}
+    	};
+    }
+
+    function instance($$self, $$props, $$invalidate) {
+    	let { $$slots = {}, $$scope } = $$props;
+
+    	$$self.$set = $$props => {
+    		if ("$$scope" in $$props) $$invalidate(0, $$scope = $$props.$$scope);
+    	};
+
+    	return [$$scope, $$slots];
+    }
+
+    class Content extends SvelteComponent {
+    	constructor(options) {
+    		super();
+    		init(this, options, instance, create_fragment$1, safe_not_equal, {});
+    	}
+    }
+
+    /* src/components/Body.svelte generated by Svelte v3.22.2 */
+
+    function create_fragment$2(ctx) {
+    	let div7;
+
+    	return {
+    		c() {
+    			div7 = element("div");
+
+    			div7.innerHTML = `<h1 class="svelte-ajlmi1">How it works?</h1> 
+  <div class="cuadrado svelte-ajlmi1"><div class="cuadrado-header svelte-ajlmi1"><div class="mac-buttons svelte-ajlmi1"><div class="mac-button close svelte-ajlmi1"></div> 
+        <div class="mac-button minimize svelte-ajlmi1"></div> 
+        <div class="mac-button maximize svelte-ajlmi1"></div></div></div> 
+    <div class="cuadrado-body svelte-ajlmi1"><img src="/img/auth.svg" alt="" class="svelte-ajlmi1"></div></div>`;
+
+    			attr(div7, "class", "contenido-container svelte-ajlmi1");
+    		},
+    		m(target, anchor) {
+    			insert(target, div7, anchor);
+    		},
+    		p: noop,
+    		i: noop,
+    		o: noop,
+    		d(detaching) {
+    			if (detaching) detach(div7);
+    		}
+    	};
+    }
+
+    class Body extends SvelteComponent {
+    	constructor(options) {
+    		super();
+    		init(this, options, null, create_fragment$2, safe_not_equal, {});
+    	}
+    }
+
+    /* src/App.svelte generated by Svelte v3.22.2 */
+
+    function create_header_slot(ctx) {
+    	let div;
+    	let current;
+    	const header = new Header({});
+
+    	return {
+    		c() {
+    			div = element("div");
+    			create_component(header.$$.fragment);
+    			attr(div, "slot", "header");
+    		},
+    		m(target, anchor) {
+    			insert(target, div, anchor);
+    			mount_component(header, div, null);
+    			current = true;
+    		},
+    		i(local) {
+    			if (current) return;
+    			transition_in(header.$$.fragment, local);
+    			current = true;
+    		},
+    		o(local) {
+    			transition_out(header.$$.fragment, local);
+    			current = false;
+    		},
+    		d(detaching) {
+    			if (detaching) detach(div);
+    			destroy_component(header);
+    		}
+    	};
+    }
+
+    // (11:2) <div slot="body">
+    function create_body_slot(ctx) {
+    	let div;
+    	let current;
+    	const body = new Body({});
+
+    	return {
+    		c() {
+    			div = element("div");
+    			create_component(body.$$.fragment);
+    			attr(div, "slot", "body");
+    		},
+    		m(target, anchor) {
+    			insert(target, div, anchor);
+    			mount_component(body, div, null);
+    			current = true;
+    		},
+    		i(local) {
+    			if (current) return;
+    			transition_in(body.$$.fragment, local);
+    			current = true;
+    		},
+    		o(local) {
+    			transition_out(body.$$.fragment, local);
+    			current = false;
+    		},
+    		d(detaching) {
+    			if (detaching) detach(div);
+    			destroy_component(body);
+    		}
+    	};
+    }
+
+    // (7:1) <Content>
+    function create_default_slot(ctx) {
+    	let t;
+
+    	return {
+    		c() {
+    			t = space();
+    		},
+    		m(target, anchor) {
+    			insert(target, t, anchor);
+    		},
+    		p: noop,
+    		i: noop,
+    		o: noop,
+    		d(detaching) {
+    			if (detaching) detach(t);
+    		}
+    	};
+    }
+
+    function create_fragment$3(ctx) {
+    	let main;
+    	let current;
+
+    	const content = new Content({
+    			props: {
+    				$$slots: {
+    					default: [create_default_slot],
+    					body: [create_body_slot],
+    					header: [create_header_slot]
+    				},
+    				$$scope: { ctx }
+    			}
+    		});
+
+    	return {
+    		c() {
+    			main = element("main");
+    			create_component(content.$$.fragment);
+    		},
+    		m(target, anchor) {
+    			insert(target, main, anchor);
+    			mount_component(content, main, null);
+    			current = true;
+    		},
+    		p(ctx, [dirty]) {
+    			const content_changes = {};
+
+    			if (dirty & /*$$scope*/ 1) {
+    				content_changes.$$scope = { dirty, ctx };
+    			}
+
+    			content.$set(content_changes);
+    		},
+    		i(local) {
+    			if (current) return;
+    			transition_in(content.$$.fragment, local);
+    			current = true;
+    		},
+    		o(local) {
+    			transition_out(content.$$.fragment, local);
+    			current = false;
+    		},
+    		d(detaching) {
+    			if (detaching) detach(main);
+    			destroy_component(content);
+    		}
+    	};
+    }
+
+    class App extends SvelteComponent {
+    	constructor(options) {
+    		super();
+    		init(this, options, null, create_fragment$3, safe_not_equal, {});
+    	}
+    }
+
+    const app = new App({
+    	target: document.body
+    });
+
+    return app;
+
+}());
+//# sourceMappingURL=bundle.js.map
