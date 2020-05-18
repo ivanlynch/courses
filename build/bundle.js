2874644cdcde25a39baed65d716a829efe027ae0
Iván Lynch <ivan.lynch@outlook.com.ar>
2020-05-17 23:11:01 -0300

    Updates

--- build/bundle.js
+++ build/bundle.js
@@ -298,14 +298,14 @@ var app = (function () {
     		c() {
     			div3 = element("div");
 
-    			div3.innerHTML = `<div class="hero-image svelte-16i9b0k"><div class="header-bar"><img class="header-logo svelte-16i9b0k" src="./img/tierra.svg" alt="Courses Market Online"> 
-      <ul class="header-menu svelte-16i9b0k"><li class="svelte-16i9b0k"><a class="header-menu-items svelte-16i9b0k" href="#/Ingresar">Ingresar</a></li> 
-        <li class="svelte-16i9b0k"><a class="header-menu-items svelte-16i9b0k" href="#/Registro">Registrarse</a></li></ul></div> 
-    <div class="hero-text svelte-16i9b0k"><h1>E-Courses</h1> 
-      <p class="hero-subtitle svelte-16i9b0k">The place where you find whatever you need</p></div> 
-    <svg class="hero-svg svelte-16i9b0k" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320"><path fill="#FFCA18" fill-opacity="1" d="M0,32L48,64C96,96,192,160,288,170.7C384,181,480,139,576,101.3C672,64,768,32,864,69.3C960,107,1056,213,1152,218.7C1248,224,1344,128,1392,80L1440,32L1440,320L1392,320C1344,320,1248,320,1152,320C1056,320,960,320,864,320C768,320,672,320,576,320C480,320,384,320,288,320C192,320,96,320,48,320L0,320Z"></path></svg></div>`;
+    			div3.innerHTML = `<div class="hero-image svelte-1nr34y2"><div class="header-bar"><img class="header-logo svelte-1nr34y2" src="./img/tierra.svg" alt="Courses Market Online"> 
+      <ul class="header-menu svelte-1nr34y2"><li class="svelte-1nr34y2"><a class="header-menu-items svelte-1nr34y2" href="#/Ingresar">Ingresar</a></li> 
+        <li class="svelte-1nr34y2"><a class="header-menu-items svelte-1nr34y2" href="#/Registro">Registrarse</a></li></ul></div> 
+    <div class="hero-text svelte-1nr34y2"><h1>E-Courses</h1> 
+      <p class="hero-subtitle svelte-1nr34y2">The place where you find whatever you need</p></div> 
+    <svg class="hero-svg svelte-1nr34y2" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320"><path fill="#FFCA18" fill-opacity="1" d="M0,32L48,64C96,96,192,160,288,170.7C384,181,480,139,576,101.3C672,64,768,32,864,69.3C960,107,1056,213,1152,218.7C1248,224,1344,128,1392,80L1440,32L1440,320L1392,320C1344,320,1248,320,1152,320C1056,320,960,320,864,320C768,320,672,320,576,320C480,320,384,320,288,320C192,320,96,320,48,320L0,320Z"></path></svg></div>`;
 
-    			attr(div3, "class", "header-container svelte-16i9b0k");
+    			attr(div3, "class", "header-container svelte-1nr34y2");
     		},
     		m(target, anchor) {
     			insert(target, div3, anchor);
